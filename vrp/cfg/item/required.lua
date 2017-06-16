bc7baa83fa0fb02aef8a295cdc1c5817d16de211
BuildTools <unconfigured@null.spigotmc.org>
2017-06-17 01:47:28 +0200

    Deleted all

--- vrp/cfg/item/required.lua
+++
@@ -1,7 +0,0 @@
-
-local items = {}
-
-items["medkit"] = {"Verbanskasten","Wird zum wiederbeleben gebraucht.",{},0.5}
-items["dirty_money"] = {"Gefälschtes Geld","lllegales Geld.",{},0}
-
-return items
